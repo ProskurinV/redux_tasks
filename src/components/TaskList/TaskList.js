835e780ae7b229fe9e5f32987433c01b741eec78
ProskurinV <proskyrinv.g@gmail.com>
2022-10-31 23:19:28 +0200

    toReducer

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,36 @@
+import { Task } from 'components/Task/Task';
+import css from './TaskList.module.css';
+
+// Импортируем хук
+import { useSelector } from 'react-redux';
+// Импортируем объект значений фильтра
+import { statusFilters } from '../../redux/constants';
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
+export const TaskList = () => {
+  // Получаем массив задач из состояния Redux
+  const tasks = useSelector(state => state.tasks);
+  // Получаем значение фильтра из состояния Redux
+  const statusFilter = useSelector(state => state.filters.status);
+  // Вычисляем массив задач которые необходимо отображать в интерфейсе
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  return (
+    <ul className={css.list}>
+      {visibleTasks.map(task => (
+        <li key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
